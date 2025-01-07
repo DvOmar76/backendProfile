bea892f214aea3435e62ba3087a4f6f1b2fe2c56
DvOmar <pagepc07@gmail.com>
2025-01-07 22:32:27 +0300

    Initial commit

---
+++ app/Http/Controllers/Auth/loginController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class loginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            return response()->json([
+//                'token' => $user->createToken('YourAppName')->plainTextToken,
+                'user' => $user
+            ]);
+        }
+
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
+}
